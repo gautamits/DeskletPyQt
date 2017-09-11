46332ee0972ad4b9179e4ef08248cf1c5161959c
gautamits <gautamamits95@gmail.com>
2017-09-11 17:13:06 +0530

    removed import warning

--- appindicator1.py
+++ appindicator1.py
@@ -5,10 +5,15 @@ import signal
 import json
 from subprocess import call
 from urllib2 import Request, urlopen, URLError
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Notify', '0.7') 
 
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
+
 
 
 APPINDICATOR_ID = 'myappindicator'
